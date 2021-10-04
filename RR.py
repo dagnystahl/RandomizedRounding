1e06600967d1a40d8390abf21722fc3e9e689f51
Dagny Stahl <dagnystahl@gmail.com>
2021-10-04 14:48:56 -0600

    template

--- RR.py
+++ RR.py
@@ -1,1 +1,48 @@
-#RANDOMIZED ROUNDING
+#############################
+#    Randomized Rounding    #
+# Dagny, Luke, Elise, Kylee #
+#############################
+
+#Imports go here
+
+###########################
+#  Part 1: Read in input  #
+###########################
+
+s = [[1,2], [1,3], [2,3]]
+w = [1,1,1]
+U = [1,2,3] #dont technically need
+
+####################################
+#    Part 2: Linear Programming    #
+# finding the partial set Solution #
+####################################
+
+# x* = simplex(...)
+def simplex_solver():
+    return 0
+
+
+###############################
+# Part 3: Randomized Rounding #
+#   finding a valid solution  #
+###############################
+
+def randomized_rounding(s, w):
+    return 0
+
+
+############################
+# Part 4: Greedy Algorithm #
+############################
+
+def greedy(s, w):
+    return 0
+
+
+####################################
+# Part 5: Trivial Random Algorithm #
+####################################
+
+def trivial_random(s, w):
+    return 0
